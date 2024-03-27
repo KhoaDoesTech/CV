cc2c684b5c9610c37ce0edee83cc385a78940313
Anh Khoa <khoahoc72@gmail.com>
2024-03-28 00:13:16 +0700

    Fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-/**
- * Entry point of application, where App is rendered within the div with the id of "app" 
- */
-
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-render(<App></App>, document.getElementById("app"));
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App></App>);
